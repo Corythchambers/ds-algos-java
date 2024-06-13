77534305f9ebd7506148736a5f4c9c84db409904
corythchambers <corythchambers@gmail.com>
2024-06-13 16:02:02 -0700

    Generic class with methods

---
+++ src/main/java/module2/MapHelper.java
@@ -0,0 +1,15 @@
+package module2;
+
+import org.testng.annotations.Test;
+
+public class MapHelper {
+
+    static <E> void showThing(E myObject) {
+        System.out.println("Here it is :\n" + myObject);
+    }
+    @Test
+    public void testGet() {
+
+    }
+
+}
---
+++ src/main/java/module2/SLList.java
@@ -0,0 +1,63 @@
+package module2;
+
+public class SLList<LochNess> {
+	class StuffNode {
+		public LochNess item;
+		public StuffNode next;
+		private StuffNode(LochNess i, StuffNode n) {
+			item = i;
+			next = n;
+		}
+	}
+	
+	public StuffNode first;
+	
+	public SLList(LochNess x) {
+		first = new StuffNode(x, null);
+	}
+	
+	public void addFirst(LochNess x) {
+		first = new StuffNode(x, first);
+	}
+	
+	public LochNess getFirst() {
+		return first.item;
+	}
+
+	public void addLast(LochNess x) {
+		StuffNode p = first;
+		while (p.next != null) {
+			p = p.next;
+		}
+		
+		p.next = new StuffNode(x, null);
+	}
+
+	private int recursiveSize(StuffNode p)
+	{
+		if(p.next == null)
+		{
+			return 1;
+		}
+		return 1+ recursiveSize(p.next);
+	}
+	public int size()
+	{
+		return recursiveSize(first);
+	}
+
+	public static void main(String[] args) {
+		SLList<Integer> L = new SLList<>(15);
+		L.addFirst(10);
+		L.addFirst(5);
+		L.addLast(20);
+		System.out.println(L.getFirst());
+
+		SLList<String> LString = new SLList<>("15");
+		LString.addFirst("aaeg");
+		LString.addFirst("5feaerwgaer");
+		LString.addLast("YESSS");
+		System.out.println(LString.getFirst());
+	 }
+
+}
